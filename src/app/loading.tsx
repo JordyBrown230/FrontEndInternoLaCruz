5ddfb7fd3202dfaeb07c16a9c395dd7d189fb5fa
Joseph2303 <106033045+Joseph2303@users.noreply.github.com>
2024-08-21 08:57:56 -0600

    caso 5

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,13 @@
-const Loading = () =>{
-    return(
-        <div>Loading</div>
-    )
-}
+import React from "react";
+import "./Loading.css";
+
+const Loading = () => {
+  return (
+    <div className="loading-container">
+      <div className="spinner"></div>
+      <div className="loading-text">Loading...</div>
+    </div>
+  );
+};
 
 export default Loading;
